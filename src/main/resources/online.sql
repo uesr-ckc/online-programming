ab3e45f3aaf4e3feb090c1234485c3e0c6d583f5
uesr-ckc <73107208+uesr-ckc@users.noreply.github.com>
2021-05-21 13:18:09 +0800

    Update online.sql

--- src/main/resources/online.sql
+++ src/main/resources/online.sql
@@ -5,7 +5,8 @@ CREATE TABLE `user`
     `password` varchar(32) NOT NULL,
     `usertype` int         NOT NULL DEFAULT '2' COMMENT '1 :管理员 2：普通用户',
     `filepath` varchar(10)          DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY (`userid`,`username`,`password`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci
